ba7947c91151270f3df8297e18e8676bcd6d3700
M <m10385263@gmail.com>
2022-05-17 13:58:24 -0400

    PRIVATE - select date by calendar, dropdown of array static data of classes based on day of week selected
    (bottom section is review data to be deleted in final, it is there to review)

--- app/src/main/java/com/example/mma/PrivateActivity.java
+++ app/src/main/java/com/example/mma/PrivateActivity.java
@@ -1,18 +1,35 @@
 package com.example.mma;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class PrivateActivity extends AppCompatActivity {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class PrivateActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, AdapterView.OnItemSelectedListener{
     private Toolbar toolbar;
+    private TextView dateTextDate;
+    private TextView dateTextWeekday;
+    private TextView dateTextClass;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +37,18 @@ public class PrivateActivity extends AppCompatActivity {
         Intent loginHomeIntent = getIntent();
         toolbar=findViewById(R.id.mmaToolBar);
         setSupportActionBar(toolbar);
+
+        dateTextDate = findViewById(R.id.dateTextDate);
+        dateTextWeekday = findViewById(R.id.dateTextWeekday);
+        dateTextClass = findViewById(R.id.dateTextClass);
+
+        findViewById(R.id.dateDialog).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDatePickerDialog();
+            }
+        });
+
     }
 
     //////////////// TOP NAV BAR MENU
@@ -92,4 +121,71 @@ public class PrivateActivity extends AppCompatActivity {
 
 
 
+    //////////////// DATE PICKER
+    private void showDatePickerDialog(){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+
+        String userMembership = "ALL";
+        String date = dayOfMonth + "-" + (month+1) + "-" + year;
+        dateTextDate.setText(date);
+        SimpleDateFormat inFormat = new SimpleDateFormat("dd-MM-yyyy");
+        String weekday="";
+
+        try {
+            Date date1 = inFormat.parse(date);
+            SimpleDateFormat outFormat = new SimpleDateFormat("EEEE");
+            weekday = outFormat.format(date1);
+            dateTextWeekday.setText(weekday);
+            Spinner spinner = findViewById(R.id.spinner);
+
+            switch(weekday){
+                case "Monday":
+                    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.privateMonday, android.R.layout.simple_spinner_item);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spinner.setAdapter(adapter);
+                    spinner.setOnItemSelectedListener(this);
+                    break;
+                case "Tuesday":
+                    ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,R.array.privateTuesday, android.R.layout.simple_spinner_item);
+                    adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spinner.setAdapter(adapter2);
+                    spinner.setOnItemSelectedListener(this);
+                    break;
+                default:
+                    ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,R.array.privateNoClasses, android.R.layout.simple_spinner_item);
+                    adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spinner.setAdapter(adapter3);
+                    spinner.setOnItemSelectedListener(this);
+                    dateTextClass.setText("ERROR");
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+        String text = parent.getItemAtPosition(position).toString();
+        dateTextClass.setText(text);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+
+
+///
 }
