3c31f2434df4f30018d29dbb9f249c49b4d5d4c0
M <m10385263@gmail.com>
2022-05-30 13:22:43 -0400

    SCHEDULE - updating array data and trainer info

--- app/src/main/java/com/example/mma/Schedule.java
+++ app/src/main/java/com/example/mma/Schedule.java
@@ -5,44 +5,48 @@ public class Schedule {
     ///TEMP DATA (MR)
     public static String[][] arrMonday = {
             {"17", "BOXING","Bob"},
-            {"18", "KICKBOXING","Rob"},
+            {"18", "MUAY THAI","Rob"},
             {"20", "PRIVATE LESSONS",""}
     };
 
     public static String[][] arrTuesday = {
             {"17", "BOXING","Bob"},
-            {"18", "KICKBOXING","Bob"},
+            {"18", "MUAY THAI","Bob"},
             {"20", "PRIVATE LESSONS",""}
     };
 
     public static String[][] arrWednesday= {
-            {"18", "KICKBOXING","Bob"},
+            {"18", "MUAY THAI","Bob"},
             {"19", "BJJ","Bill"},
             {"20", "PRIVATE LESSONS",""}
     };
 
     public static String[][] arrThursday = {
+            {"16", "WRESTLING","Will"},
             {"17", "BOXING","Bob"},
-            {"18", "KICKBOXING","Rob"},
+            {"18", "MUAY THAI","Rob"},
             {"19", "BJJ","Will"},
             {"20", "PRIVATE LESSONS",""}
     };
 
     public static String[][] arrFriday = {
-            {"17", "BOXING","Bob"},
-            {"18", "KICKBOXING","Rob"},
+            {"16", "WRESTLING","Bob"},
+            {"17", "BOXING","Will"},
+            {"18", "MUAY THAI","Rob"},
             {"19", "BJJ","Will"},
             {"20", "PRIVATE LESSONS",""}
     };
 
     public static String[][] arrSaturday = {
             {"12", "BOXING","Bob"},
-            {"13", "BJJ","Will"}
+            {"13", "BJJ","Will"},
+            {"14", "WRESTLING","Will"},
     };
 
     public static String[][] arrSunday = {
             {"12", "BOXING","Bob"},
-            {"13", "BJJ","Will"}
+            {"13", "BJJ","Will"},
+            {"14", "WRESTLING","Will"},
     };
 
 
